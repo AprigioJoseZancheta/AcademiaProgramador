0abcc748cc3e5c22a2fda9881e358b1d463a9d07
AprigioJoseZancheta <prijotricolor@hotmail.com>
2014-07-28 20:09:22 -0300

    Comitando

---
+++ Academia/Unidade1/ExerciciosFixacao/Exercicio6.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade1.ExerciciosFixacao
+{
+    /*Menu padrão*/
+  class Exercicio6
+  {
+        static void Main()
+        {
+            Menu();
+        }
+    
+    public static void Menu6()
+    {
+        string opcao = "";
+        Console.WriteLine(" MENU\n Digite(1) Padrão1\n Digite(2) Padrão2\n Digite(3) Padrão3\n Digite(4) Padrão4");
+        opcao = Console.ReadLine();
+        switch (opcao)
+        {
+            case "1": Console.WriteLine("Padrão 1");
+                break;
+            case "2": Console.WriteLine("Padrão 2");
+                break;
+            case "3": Console.WriteLine("Padrão 3");
+                break;
+            case "4": Console.WriteLine("Padrão 4");
+                break;
+            default: Console.WriteLine("Opção Inválida");
+                break;
+        }
+        Console.ReadKey();
+    }
+ }
+}
