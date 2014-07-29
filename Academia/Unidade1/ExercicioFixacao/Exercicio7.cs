fff24fb8d7e32e83de2497d803c94cec4ebb9a7b
AprigioJoseZancheta <prijotricolor@hotmail.com>
2014-07-29 15:07:17 -0300

    Comitando

---
+++ Academia/Unidade1/ExercicioFixacao/Exercicio7.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidade1.ExercicioFixacao
+{
+    /*Serie Fibinacci*/
+    class Exercicio7
+    {
+        static void Main7()
+        {
+            int novoNumero = 0;
+            int numeroAtual = 1;
+            int numeroAnterior = 0;
+
+            for (int i = 0; i < 30; i++)
+            {
+                novoNumero = numeroAnterior + numeroAtual;
+                numeroAnterior = numeroAtual;
+                numeroAtual = novoNumero;
+                Console.WriteLine("O na posição " + i + " número de fibonacci      " + numeroAtual);
+            }
+            Console.ReadKey();
+        }
+    }
+}
